6fd0db79eea032274ca97d9fa5ea732427695777
Rohit <rohit.krishnan@gmail.com>
2023-12-22 22:38:58 -0800

    Update results_convert.py

--- results_convert.py
+++ results_convert.py
@@ -1,3 +1,4 @@
+import re
 import pandas as pd
 from difflib import SequenceMatcher
 
@@ -9,14 +10,14 @@ def clean_text(text):
 
 def create_combined_csv(original_csv_path, interim_csv_path, combined_csv_path):
     # Read the original and interim data
-    original_data = pd.read_csv(original_csv_path, encoding='utf-8-sig')
-    interim_data = pd.read_csv(interim_csv_path, encoding='utf-8-sig')
+    original_data = pd.read_excel(original_csv_path) #, encoding='utf-8-sig'
+    interim_data = pd.read_excel(interim_csv_path)
 
     # Combine the data
     combined_data = pd.concat([original_data, interim_data], ignore_index=True)
 
     # Save the combined data to a new CSV file
-    combined_data.to_csv(combined_csv_path, index=False)
+    combined_data.to_excel(combined_csv_path, index=False)
 
 def merge_on_contains(big_df, small_df, big_col, small_col):
     # Lowercase and strip whitespace for more effective matching
@@ -39,24 +40,44 @@ def merge_on_contains(big_df, small_df, big_col, small_col):
 
     return big_df
 
-# Load the CSV files
-questions_file_path = 'questions.csv' 
-llmresults_file_path = 'results_grouped_by_model.csv'  # File path for results
-gpt4results_csv_path = 'results_gpt4.csv'
-results_file_path = 'allresults_grouped_by_model.csv'  # File path for results
+# Load the files
+questions_file_path = 'questions.xlsx' 
+llmresults_file_path = 'results_grouped_by_model.xlsx'
+gpt4results_csv_path = 'results_gpt4.xlsx'
+results_file_path = 'allresults_grouped_by_model.xlsx'
 
 create_combined_csv(llmresults_file_path, gpt4results_csv_path, results_file_path)
 
-# Reading the CSV files
-questions_df = pd.read_csv(questions_file_path)
-results_df = pd.read_csv(results_file_path)
+# Reading the files
+questions_df = pd.read_excel(questions_file_path)
+results_df = pd.read_excel(results_file_path)
+
+# Ensure the total number of questions in results_grouped_by_model.xlsx is a multiple of the number in questions.xlsx
+if len(results_df) % len(questions_df) != 0:
+    raise ValueError("The total number of questions in results_grouped_by_model.xlsx must be a multiple of the number in questions.xlsx.")
+
+# Replace questions in results_grouped_df with those from questions_df
+num_repetitions = len(results_df) // len(questions_df)
+repeated_questions = pd.concat([questions_df['Question']] * num_repetitions, ignore_index=True)
+results_df['Question'] = repeated_questions
+
+# Save the modified DataFrame to a new Excel file
+results_df.to_excel(results_file_path, index=False)  # Replace with your desired path
 
 # Applying the merge_on_contains function
 merged_df = merge_on_contains(results_df, questions_df, 'Question', 'Question')
 
-merged_df['Question'] = merged_df['Question'].str.replace(r'\\"', '', regex=True)
-merged_df['Question'] = merged_df['Question'].str.replace(r'"', '', regex=True)
-merged_df['Question'] = merged_df['Question'].apply(clean_text)
+# CLEAN DATA
+# # Define a regular expression pattern to match unwanted characters and sequences
+# pattern = r'(\\n|\\|")'
+# # Use the pattern to replace matched characters with a single space
+# merged_df['Question'] = merged_df['Question'].str.replace(pattern, ' ', regex=True)
+# # Apply the clean_text function to remove non-ASCII characters
+# merged_df['Question'] = merged_df['Question'].apply(clean_text)
+# # Replace multiple white spaces with a single space
+# merged_df['Question'] = merged_df['Question'].str.replace(r'\s+', ' ', regex=True)
+# # Remove leading and trailing whitespaces
+# merged_df['Question'] = merged_df['Question'].str.strip()
 
 # Pivoting the data
 pivoted_data = merged_df.pivot(index=['Question', 'category'], columns='Model', values='Response')
@@ -64,4 +85,4 @@ pivoted_data = merged_df.pivot(index=['Question', 'category'], columns='Model', 
 # Resetting index to make 'Question' and 'category' columns again
 pivoted_data.reset_index(inplace=True)
 
-pivoted_data.to_csv('results_grouped_by_question.csv', index=False)
+pivoted_data.to_excel('results_grouped_by_question.xlsx', index=False)
