029908349ba48ee01b6a4e4c16322cbbff008600
Rohit <rohit.krishnan@gmail.com>
2024-01-30 12:29:28 -0800

    updates

--- config.py
+++ config.py
@@ -1,40 +1,62 @@
 import json
-# filepath
-config_file = 'config.json'
-with open(config_file, 'r') as config_file:
-    config = json.load(config_file)
 
-INSTRUCTION = config['instructions']
-F_NAME = config["name"]
-GPT_MODEL = config["GPT_MODEL"]
+class Config:
+    def __init__(self):
+        self.config_file = 'config.json'
+        self.config = self.load_config()
+        self.F_NAME = self.config["name"]
+        self.INSTRUCTION = self.config['instructions']
+        self.GPT_MODEL = self.config["GPT_MODEL"]
+        self.perturbations = 'utils/perturbations.json'
+        self.knowledgebase = 'utils/knowledgebase.json'
+        self.db_layout = 'utils/database_description.json'
+        # Define file paths for each mode
+        self.file_paths = {
+            'default': {
+                'questions': 'files/questions.xlsx',
+                'q_original': 'files/questions_original.xlsx',
+                'llmresults_file_path': f'files/{self.F_NAME}_results_grouped_by_model.xlsx',
+                'gpt4results_csv_path': f'files/{self.F_NAME}_results_gpt4.xlsx',
+                'results_file_path': f'files/{self.F_NAME}_allresults_grouped_by_model.xlsx',
+                'combined_file_path': f'files/{self.F_NAME}_results_grouped_by_question.xlsx',
+                'llmeval_results': f'{self.F_NAME}_llmeval_results.xlsx',
+                'model_rankings': f'{self.F_NAME}_model_rankings.xlsx',
+            },
+            'db': {
+                'questions': 'files/questions_db.xlsx',
+                'q_original': 'files/questions_original_db.xlsx',
+                'llmresults_file_path': f'files/{self.F_NAME}_results_grouped_by_model_db.xlsx',
+                'gpt4results_csv_path': f'files/{self.F_NAME}_results_gpt4_db.xlsx',
+                'results_file_path': f'files/{self.F_NAME}_allresults_grouped_by_model_db.xlsx',
+                'combined_file_path': f'files/{self.F_NAME}_results_grouped_by_question_db.xlsx',
+                'llmeval_results_db': f'{self.F_NAME}_llmeval_results_db.xlsx',
+                'model_rankings_db': f'{self.F_NAME}_model_rankings_db.xlsx',
+            },
+            'dynamic': {
+                'questions': 'files/questions_dynamic.xlsx',
+                'q_original': 'files/questions_original_dynamic.xlsx',
+                'llmresults_file_path': f'files/{self.F_NAME}_results_grouped_by_model_dynamic.xlsx',
+                'gpt4results_csv_path': f'files/{self.F_NAME}_results_gpt4_dynamic.xlsx',
+                'results_file_path': f'files/{self.F_NAME}_allresults_grouped_by_model_dynamic.xlsx',
+                'combined_file_path': f'files/{self.F_NAME}_results_grouped_by_question_dynamic.xlsx',
+                'llmeval_results_dy': f'{self.F_NAME}_llmeval_results_dynamic.xlsx',
+                'model_rankings_dy': f'{self.F_NAME}_model_rankings_dynamic.xlsx',
+            }
+        }
+        self.current_mode = None
+        self.set_mode('default')  # Set default mode initially
 
-perturbations = 'utils/perturbations.json'
-knowledgebase = 'utils/knowledgebase.json'
-db_layout = 'utils/database_description.json'
+    def load_config(self):
+        with open(self.config_file, 'r') as file:
+            return json.load(file)
 
-questions = 'files/questions.xlsx'
-q_original = 'files/questions_original.xlsx'
-llmresults_file_path = f'files/{F_NAME}_results_grouped_by_model.xlsx'
-gpt4results_csv_path = f'files/{F_NAME}_results_gpt4.xlsx'
-results_file_path = f'files/{F_NAME}_allresults_grouped_by_model.xlsx'
-combined_file_path = f'files/{F_NAME}_results_grouped_by_question.xlsx'
-llmeval_results = f'{F_NAME}_llmeval_results.xlsx'
-model_rankings = f'{F_NAME}_model_rankings.xlsx'
+    def set_mode(self, mode):
+        if mode in self.file_paths and mode != self.current_mode:
+            self.current_mode = mode
+            for key, value in self.file_paths[mode].items():
+                setattr(self, key, value)
+        else:
+            raise ValueError(f"Invalid mode: {mode}")
 
-q_dynamic = 'files/questions_dynamic.xlsx'
-q_orig_dynamic = 'files/questions_original_dynamic.xlsx'
-llmresults_file_path_dy = f'files/{F_NAME}_results_grouped_by_model_dynamic.xlsx'
-gpt4results_csv_path_dy = f'files/{F_NAME}_results_gpt4_dynamic.xlsx'
-results_file_path_dy = f'files/{F_NAME}_allresults_grouped_by_model_dynamic.xlsx'
-combined_file_path_dy = f'files/{F_NAME}_results_grouped_by_question_dynamic.xlsx'
-llmeval_results_dy = f'{F_NAME}_llmeval_results_dynamic.xlsx'
-model_rankings_dy = f'{F_NAME}_model_rankings_dynamic.xlsx'
-
-q_db = 'files/questions_db.xlsx'
-q_orig_db = 'files/questions_original_db.xlsx'
-llmresults_file_path_db = f'files/{F_NAME}_results_grouped_by_model_db.xlsx'
-gpt4results_csv_path_db = f'files/{F_NAME}_results_gpt4_db.xlsx'
-results_file_path_db = f'files/{F_NAME}_allresults_grouped_by_model_db.xlsx'
-combined_file_path_db = f'files/{F_NAME}_results_grouped_by_question_db.xlsx'
-llmeval_results_db = f'{F_NAME}_llmeval_results_db.xlsx'
-model_rankings_db = f'{F_NAME}_model_rankings_db.xlsx'
+# Create a global instance
+config = Config()
