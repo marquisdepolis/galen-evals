e00963ba704ee5e15df3d56ea3dc9cd622b649c6
Rohit <rohit.krishnan@gmail.com>
2024-02-12 16:38:49 -0800

    Multiple Updates

--- 1.6_archive_files.py
+++ 1.6_archive_files.py
@@ -11,7 +11,7 @@ if not os.path.exists(archive_directory):
 
 # List all files that start with F_NAME and exclude the specified files
 files_to_move = [f for f in glob.glob(f"{directory}/{config.F_NAME}_*") 
-                 if '_model_rankings' not in f and '_llmeval_results' not in f and 'questions' not in f and '_results_grouped_by_question_' not in f]
+                 if '_model_rankings' not in f and '_llmeval_results' not in f and 'questions' not in f and '_results_grouped_by_question_' not in f and '_allresults_grouped_by_model_' not in f]
 
 # Move the files to the #Archive folder
 for file in files_to_move:
