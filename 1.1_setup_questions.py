7beeff56e54edfe8a10ca381b01c6b8a50a77887
Rohit <rohit.krishnan@gmail.com>
2024-02-16 14:19:26 -0800

    updates

--- 1.1_setup_questions.py
+++ 1.1_setup_questions.py
@@ -1,7 +1,8 @@
 import pandas as pd
-
+from config import config
+config.set_mode("default")
 # Load the questions from the original Excel file
-questions_df = pd.read_excel('questions.xlsx')
+questions_df = pd.read_excel('files/questions.xlsx')
 
 # Filter questions based on the 'Type' column
 rag_questions = questions_df[questions_df['Type'] == 'RAG']
@@ -9,6 +10,6 @@ db_questions = questions_df[questions_df['Type'] == 'DB']
 dynamic_questions = questions_df[~questions_df['Type'].isin(['RAG', 'DB'])]
 
 # Save the filtered questions into separate Excel files
-rag_questions.to_excel('questions_rag.xlsx', index=False)
-db_questions.to_excel('questions_db.xlsx', index=False)
-dynamic_questions.to_excel('questions_dynamic.xlsx', index=False)
+rag_questions.to_excel('files/questions_rag.xlsx', index=False)
+db_questions.to_excel('files/questions_db.xlsx', index=False)
+dynamic_questions.to_excel('files/questions_dynamic.xlsx', index=False)
